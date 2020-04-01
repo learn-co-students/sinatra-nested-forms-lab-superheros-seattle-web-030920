540a97232c72781c52bc3a0ba1420c6a683f3561
jeff_simon <jeffsbillinfo@gmail.com>
2020-04-01 14:40:58 -0700

    made some changes to controller, views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,19 @@
 require 'sinatra/base'
+require 'pry'
 
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+        erb :super_hero
+    end
 
+    post '/teams' do
+        #@team = params[:team]
+        @team = params["team"]["name"]
+        @motto = params["team"]["motto"]
+        binding.pry
+        erb :team
+    end
 end
