05c2ffd89bb0eacfeff6701dac18e57c551fb26b
jeff_simon <jeffsbillinfo@gmail.com>
2020-04-01 16:11:15 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,15 +10,8 @@ class App < Sinatra::Base
     end
 
     post '/teams' do
-        #@team = params[:team]
         @team = params["team"]["name"]
         @motto = params["team"]["motto"]
-
-        # @hero_name = []
-
-        # @hero_power = []
-        # @hero_bio = []
-
         @team_members = params["team"]["members"]
         
         erb :team
